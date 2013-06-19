bed849c68306bdff12dba14aa7d1b24a7b106092
Bram <bwasti@gmail.com>
2013-06-18 20:18:33 -0400

    Initial Commit

---
+++ CameraControl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CameraControl
+//
+//  Created by Bram Wasti on 6/18/13.
+//  Copyright (c) 2013 bwasti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CameraControlTests/CameraControlTests.m
@@ -0,0 +1,36 @@
+//
+//  CameraControlTests.m
+//  CameraControlTests
+//
+//  Created by Bram Wasti on 6/18/13.
+//  Copyright (c) 2013 bwasti. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CameraControlTests : XCTestCase
+
+@end
+
+@implementation CameraControlTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
